c162204ac85621f6c440e3656f7ced966b51f164
Efim Zabarsky <Efim.Zabarsky@kla-tencor.com>
2023-01-15 12:40:20 +0200

    finished worker pool

---
+++ 3_concurrencyPatterns/2. worker Pool.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"log"
+	"time"
+)
+
+func main() {
+	numOfJobs := 5
+	jobs := make(chan int, numOfJobs)
+	results := make(chan int, numOfJobs)
+
+	defer close(jobs)
+	defer close(results)
+
+	for i := 0; i < 3; i++ {
+		go workerSingle(i*10, jobs, results)
+	}
+
+	time.Sleep(time.Second)
+
+	for i := 0; i < 3; i++ {
+		log.Println("Push job! i = ", i)
+		jobs <- i
+	}
+
+	for i := 0; i < 3; i++ {
+		log.Println("Got result! i = ", <-results)
+	}
+}
+
+func workerSingle(id int, jobs chan int, results chan int) {
+	log.Println(id, " worker single ready!")
+	i := <-jobs
+	log.Println(id, " worker single got job to do! Payload = ", i)
+	time.Sleep(time.Second)
+	results <- i + 100
+}
+
+func workerRange(id int, jobs chan int, results chan int) {
+	log.Println(id, " worker range ready!")
+	for i := range jobs {
+		log.Println(id, " worker range got job to do! Payload = ", i)
+		time.Sleep(time.Second)
+		results <- i + 100
+	}
+}
